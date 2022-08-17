f0f4af2ba31ef0e1adb271e6414bf89caaac777a
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-17 20:01:42 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,17 @@
 class Dog
     # setter method
-    def name=(dog_name)
+    attr_reader:dog_name
+    def initialize (dog_name)
         @this_dogs_name = dog_name
     end
     # getter method
-    def name
-        @this_dogs_name
-    end
+    # def name
+    #     @this_dogs_name
+    # end
 end
 
-lassie = Dog.new
+lassie = Dog.new(Lassie)
 # sets the name 
-lassie.name = "Lassie"
+#assie.name = ""
 # gets the name
 puts lassie.name
